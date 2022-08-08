403cf5fc4c05af2f28998284494ecf6d7eff5966
rokas <rokasra@gmail.com>
2022-08-08 15:09:51 +0300

    callback example

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
+import { useState } from 'react';
 import './App.css';
+import Callback from './callback';
+
+// Parent
 
 function App() {
+  const [UIcolor, setUIColor] = useState(null);
+
+  // our callback function
+  const getColor = (color) => {
+    setUIColor(color);
+  };
   return (
     <div className='App'>
-      <h1>Heloo again world</h1>
+      <div className='colorContainer' style={{ background: `${UIcolor}` }}></div>
+      <Callback getColor={getColor} />
     </div>
   );
 }
